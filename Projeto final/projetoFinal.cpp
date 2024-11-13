94d5bb28ea07961aa37c75a7bc7e21b7b36f544d
Flobster23 <101504463+Flobster23@users.noreply.github.com>
2024-11-13 08:47:15 -0300

    Add files via upload

---
+++ Projeto final/projetoFinal.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void imprimeCabecalho(){
+    cout << "**********************************************************" << endl;
+    cout << "Bem vindo ao sistema de gerenciamento das palhas do Bizzi!" << endl;
+    cout << "**********************************************************" << endl;
+}
+
+int main(){
+    imprimeCabecalho();
+    
+    return 0;
+}
